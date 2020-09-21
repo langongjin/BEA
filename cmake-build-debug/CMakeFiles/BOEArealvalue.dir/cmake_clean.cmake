d67301193a582fee52b090eac2be63a00e8ec344
langongjin <lanmessi@hotmail.com>
2020-09-21 14:55:34 +0200

    Add files via upload

---
+++ cmake-build-debug/CMakeFiles/BOEArealvalue.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/BOEArealvalue.dir/main.cpp.o"
+  "BOEArealvalue.pdb"
+  "BOEArealvalue"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/BOEArealvalue.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
